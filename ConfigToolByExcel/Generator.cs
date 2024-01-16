f8e9fdd6c866432f9dcad3645f0571fa40247a92
EdenMeng <liulimengplay@163.com>
2024-01-16 17:31:51 +0800

    读取Excel文件时读取目录下的所有excel文件

--- ConfigToolByExcel/Generator.cs
+++ ConfigToolByExcel/Generator.cs
@@ -1,26 +1,50 @@
-﻿namespace ConfigToolByExcel
+﻿using System.IO;
+
+namespace ConfigToolByExcel
 {
     public class Generator
     {
-        // 生成代码文件
+        /// <summary>
+        /// 生成代码文件
+        /// </summary>
+        /// <param name="excelFilePath">Excel文件所在目录的路径</param>
+        /// <param name="codeOutputFolderPath">生成的代码文件的输出路径</param>
         public static void GenerateClass(string excelFilePath, string codeOutputFolderPath)
         {
-            var classes = ClassReader.CollectClassesInfo(excelFilePath);
-            if (classes != null)
+            var fileFullPaths = Directory.GetFiles(excelFilePath);
+            foreach (var fullPath in fileFullPaths)
             {
-                foreach (var classInfo in classes)
-                    ClassFileGenerator.GenerateClassFile(classInfo, codeOutputFolderPath);
+                if (!fullPath.EndsWith(".xlsx"))
+                    continue;
+
+                var classes = ClassReader.CollectClassesInfo(fullPath);
+                if (classes != null)
+                {
+                    foreach (var classInfo in classes)
+                        ClassFileGenerator.GenerateClassFile(classInfo, codeOutputFolderPath);
+                }
             }
         }
 
-        // 将配置数据转换为json文件
+        /// <summary>
+        /// 将配置数据转换为json文件
+        /// </summary>
+        /// <param name="excelFilePath">Excel文件所在目录的路径</param>
+        /// <param name="dataOutputFolderPath">生成的数据Json文件的输出路径</param>
         public static void GenerateData(string excelFilePath, string dataOutputFolderPath)
         {
-            var datas = ClassReader.CollectNumeric(excelFilePath);
-            if (datas != null)
+            var fileFullPaths = Directory.GetFiles(excelFilePath);
+            foreach (var fullPath in fileFullPaths)
             {
-                foreach (var data in datas)
-                    NumericFileGenerator.GenerateNumericFile(data.Key, data.Value, dataOutputFolderPath);
+                if (!fullPath.EndsWith(".xlsx"))
+                    continue;
+
+                var datas = ClassReader.CollectNumeric(fullPath);
+                if (datas != null)
+                {
+                    foreach (var data in datas)
+                        NumericFileGenerator.GenerateNumericFile(data.Key, data.Value, dataOutputFolderPath);
+                }
             }
         }
     }
