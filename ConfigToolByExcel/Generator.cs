f683a5756730cceea18e928f753b4737ead2b6ee
EdenMeng <liulimengplay@163.com>
2024-01-16 13:41:12 +0800

    将调用代码拆分为两个，分别生成类文件和json文件

--- ConfigToolByExcel/Generator.cs
+++ ConfigToolByExcel/Generator.cs
@@ -2,24 +2,25 @@
 {
     public class Generator
     {
-        public static void Generate(string excelFilePath, string codeOutputFolderPath, string dataOutputFolderPath)
+        // 生成代码文件
+        public static void GenerateClass(string excelFilePath, string codeOutputFolderPath)
         {
-            // 步骤1.生成代码文件
             var classes = ClassReader.CollectClassesInfo(excelFilePath);
             if (classes != null)
             {
                 foreach (var classInfo in classes)
                     ClassFileGenerator.GenerateClassFile(classInfo, codeOutputFolderPath);
             }
+        }
 
-            // 步骤2.将配置数据转换为json文件
+        // 将配置数据转换为json文件
+        public static void GenerateData(string excelFilePath, string dataOutputFolderPath)
+        {
             var datas = ClassReader.CollectNumeric(excelFilePath);
             if (datas != null)
             {
                 foreach (var data in datas)
-                {
                     NumericFileGenerator.GenerateNumericFile(data.Key, data.Value, dataOutputFolderPath);
-                }
             }
         }
     }
